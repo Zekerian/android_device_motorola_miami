195bac4922bbe88c19bb8b3ff9d442849d808409
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-05 17:26:29 +0530

    miami: Add support for high brightness mode (HBM)
    Brightness from 3514 (600 nits) to 4095 (700 nits) is HBM.
    Enter HBM at >=6500 lux (outdoor daylight) and hit peak brightness
    at >=10000 lux (under direct sunlight). Adjust the backlight curve
    accordingly.
    
    Requires increasing max brightness to 4095 in kernel.
    
    Original patch ported from dubai, used values from rhodep stock:
    https://github.com/ArrowOS-Devices/android_device_motorola_dubai/commit/db7b0643f28d45d76da7f0ff3d559d5330b0a69a
    
    Change-Id: Ie7f964a615f859bfd8d07937dd7b6bbd0fdc2a42
    Signed-off-by: Sevenrock <sevenrock@hotmail.de>

--- device.mk
+++ device.mk
@@ -35,6 +35,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libgui_shim_vendor
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630947043778501761.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947043778501761.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.miami \
